d80cce0a9692ff6b088503f048cd577e49341329
Philippe Beck <philippe@philippebeck.net>
2021-05-09 16:13:52 +0200

    Create new controllers

---
+++ src/Controller/ActivityController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Pam\Controller\MainController;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+/**
+ * Class ActivityController
+ * @package App\Controller
+ */
+class ActivityController extends MainController
+{
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function defaultMethod()
+    {
+        return $this->render("front/activity.twig");
+    }
+}
